bb02047450feae8603d9910187c9c67965dd3dd5
bema <d00230369@student.dkit.ie>
2026-02-24 02:20:24 +0000

    booking dao

---
+++ src/main/java/SpringProject/persistences/BookingsDao.java
@@ -0,0 +1,25 @@
+package SpringProject.persistences;
+
+import SpringProject.dtos.Bookings;
+
+import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+/**bookingId INT AUTO_INCREMENT PRIMARY KEY,
+driverId INT NOT NULL,
+userId INT NOT NULL,
+carId INT NOT NULL,
+pickupDatetime DATETIME NOT NULL,
+returnDatetime DATETIME NOT NULL,
+pickupLocationId INT,
+totalPrice DECIMAL(10, 2),
+status ENUM('confirmed', 'active', 'returned', 'cancelled') DEFAULT 'confirmed',
+ */
+public interface BookingsDao {
+    List<Bookings> findAllBookings() throws SQLException;
+    Bookings getBookingsById(int bookingId) throws SQLException;
+    boolean deleteBooking(int bookingId) throws SQLException;
+    boolean AddBookings(Bookings bookings) throws SQLException;
+    boolean updateBooking(Bookings booking) throws SQLException;
+
+}
