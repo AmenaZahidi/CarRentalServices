d4dc25c952779c82fb7354c36a1a47c6c7fd5595
bema <d00230369@student.dkit.ie>
2026-02-24 13:17:03 +0000

    Booking dto

---
+++ src/main/java/SpringProject/dtos/Bookings.java
@@ -0,0 +1,40 @@
+package SpringProject.dtos;
+/*CREATE TABLE bookings (
+        bookingId INT AUTO_INCREMENT PRIMARY KEY,
+        driverId INT NOT NULL,
+        userId INT NOT NULL,
+        carId INT NOT NULL,
+        pickupDatetime DATETIME NOT NULL,
+        returnDatetime DATETIME NOT NULL,
+        pickupLocationId INT,
+        totalPrice DECIMAL(10, 2),
+status ENUM('confirmed', 'active', 'returned', 'cancelled') DEFAULT 'confirmed',
+
+FOREIGN KEY (userId) REFERENCES users(userId),
+FOREIGN KEY (carId) REFERENCES carDetails(carId),
+FOREIGN KEY (driverId) REFERENCES driverdetails(driverId),
+FOREIGN KEY (pickupLocationId) REFERENCES location(locationId)
+        );*/
+import lombok.*;
+
+import java.util.Date;
+
+@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Bookings {
+    @NonNull
+    private int bookingId;
+    @NonNull
+    private Date pickupDateTime;
+    @NonNull
+    private Date returnDateTime;
+
+    private double totalPrice;
+    @NonNull
+    private String status;
+}
