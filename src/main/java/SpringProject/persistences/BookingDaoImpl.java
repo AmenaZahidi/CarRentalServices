6c565d390a3c12d0702dd188c3059d835e0b0d2f
bema <d00230369@student.dkit.ie>
2026-03-10 07:12:50 +0000

    delete, add, update, get methods

--- src/main/java/SpringProject/persistences/BookingDaoImpl.java
+++ src/main/java/SpringProject/persistences/BookingDaoImpl.java
@@ -48,23 +48,97 @@ public BookingDaoImpl(Connector connector) {this.connector = connector;}
 
     @Override
     public Bookings getBookingsById(int bookingId) throws SQLException {
+        Connection conn = connector.getConnection();
+        if (conn == null) throw new SQLException("getBookingsById(): Could not establish connection to database.");
+
+        String sql = """
+                SELECT bookingId, pickupDateTime, returnDateTime, status, totalPrice
+                FROM Bookings
+                WHERE bookingId = ?
+                """;
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, bookingId);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) return mapBookingsRow(rs);
+            }
+
+        } catch (SQLException e) {
+            log.error("getBookingsById(): SQL error: {}", e.getMessage());
+            throw e;
+        } finally {
+            connector.freeConnection();
+        }
+
         return null;
     }
 
     @Override
     public boolean deleteBooking(int bookingId) throws SQLException {
+        Connection conn = connector.getConnection();
+        String sql="DELETE FROM users WHERE bookingId =?";
+        try(PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setInt(1, bookingId);
+            return ps.executeUpdate()>0;
+        }catch(SQLException e){
+            log.info("deleteBooking(): SQL Exception occurred when attempting to prepare SQL for execution" + e.getMessage());
+        }
         return false;
     }
 
     @Override
     public boolean AddBookings(Bookings bookings) throws SQLException {
-        return false;
+        boolean added = false;
+        conn = connector.getConnection();
+
+        if (bookings == null) {
+            throw new IllegalArgumentException("Booking cant be null");
+        }
+        if (connector == null) {
+            throw new SQLException("Could not establish connection to database");
+        }
+        int addedRows = 0;
+        String sql = "INSERT INTO bookings (bookingId, pickupDateTime, returnDateTime, status, totalPrice) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, bookings.getBookingId());
+            ps.setTimestamp(2, new java.sql.Timestamp(bookings.getPickupDateTime().getTime()));
+            ps.setTimestamp(3,new java.sql.Timestamp(bookings.getReturnDateTime().getTime()));
+            ps.setString(4, bookings.getStatus());
+            ps.setDouble(5, bookings.getTotalPrice());
+            addedRows = ps.executeUpdate();
+        }
+        catch (SQLException e){
+            log.error(("The SQL query could not be executed: " + e.getMessage()));
+            throw e;
+        }
+        return addedRows ==1;
     }
 
     @Override
-    public boolean updateBooking(Bookings booking) throws SQLException {
-        return false;
+    public boolean updateBooking(Bookings bookings) throws SQLException {
+        Connection conn = connector.getConnection();
+        if (conn == null) throw new SQLException("updateBookingStatus(): Could not establish connection to database.");
+
+        String sql = "UPDATE Bookings SET pickupDateTime = ?, returnDateTime= ?, totalPrice = ?, status = ? " +
+                " WHERE bookingId = ?";
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, bookings.getBookingId());
+            ps.setTimestamp(2, new java.sql.Timestamp(bookings.getPickupDateTime().getTime()));
+            ps.setTimestamp(3,new java.sql.Timestamp(bookings.getReturnDateTime().getTime()));
+            ps.setString(4, bookings.getStatus());
+            ps.setDouble(5, bookings.getTotalPrice());
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            log.error("updateBookingStatus(): SQL error: {}", e.getMessage());
+            throw e;
+        } finally {
+            connector.freeConnection();
+        }
     }
+
 
     private static Bookings mapBookingsRow(ResultSet rs) throws SQLException{
         return Bookings.builder()
